e2bef497cadcc56044ea39a05ffe5e2bee099999
786khk <78067072+786khk@users.noreply.github.com>
2022-12-20 21:40:00 +0900

    init

---
+++ JAVA/Test.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test{
+    public static void main(String[] agrs){
+        
+        // String[] s1 = {"a", "b", "c"};
+        // String[] s2 = {"com", "b", "d", "p", "c"};
+        Solution();
+    }
+
+    public static int Solution(){
+        // n이하의 숫자중 홀수만 answer에 담기
+        int answer = 0;
+        int[] arr = new int[5];
+        int[] value = {10,20,30};
+
+        List list = new ArrayList();
+        list.add("월");
+        list.add("화");
+        list.add("수");
+        
+        list.remove(0);
+        return answer;
+    }
+}
